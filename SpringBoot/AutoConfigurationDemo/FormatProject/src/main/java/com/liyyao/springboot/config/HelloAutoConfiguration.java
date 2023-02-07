dbf843e23fcecef4975a6da50f794413b142265e
liyyao <2813789717@qq.com>
2023-02-07 17:21:56 +0800

    自动装配-手写Starter-添加属性配置

--- SpringBoot/AutoConfigurationDemo/FormatProject/src/main/java/com/liyyao/springboot/config/HelloAutoConfiguration.java
+++ SpringBoot/AutoConfigurationDemo/FormatProject/src/main/java/com/liyyao/springboot/config/HelloAutoConfiguration.java
@@ -1,17 +1,20 @@
 package com.liyyao.springboot.config;
 
+import com.liyyao.springboot.properties.HelloProperties;
 import com.liyyao.springboot.service.FormatProcessor;
 import com.liyyao.springboot.util.HelloFormatTemplate;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(FormatAutoConfiguration.class)
+@EnableConfigurationProperties(HelloProperties.class)
 public class HelloAutoConfiguration {
 
     @Bean
-    public HelloFormatTemplate helloFormatTemplate(FormatProcessor formatProcessor) {
-        return new HelloFormatTemplate(formatProcessor);
+    public HelloFormatTemplate helloFormatTemplate(HelloProperties helloProperties, FormatProcessor formatProcessor) {
+        return new HelloFormatTemplate(helloProperties, formatProcessor);
     }
 }
--- SpringBoot/AutoConfigurationDemo/FormatProject/src/main/java/com/liyyao/springboot/util/HelloFormatTemplate.java
+++ SpringBoot/AutoConfigurationDemo/FormatProject/src/main/java/com/liyyao/springboot/util/HelloFormatTemplate.java
@@ -1,18 +1,23 @@
 package com.liyyao.springboot.util;
 
+import com.liyyao.springboot.properties.HelloProperties;
 import com.liyyao.springboot.service.FormatProcessor;
 
 public class HelloFormatTemplate {
 
     private FormatProcessor formatProcessor;
 
-    public HelloFormatTemplate(FormatProcessor processor) {
+    private HelloProperties helloProperties;
+
+    public HelloFormatTemplate(HelloProperties helloProperties, FormatProcessor processor) {
         this.formatProcessor = processor;
+        this.helloProperties = helloProperties;
     }
 
     public <T> String doFormat(T obj) {
         StringBuilder builder = new StringBuilder();
         builder.append("Execute format : ").append("<br>");
+        builder.append("HelloProperties : ").append(formatProcessor.format(helloProperties.getInfo())).append("<br>");
         builder.append("Object format result : ").append(formatProcessor.format(obj));
         return builder.toString();
     }
